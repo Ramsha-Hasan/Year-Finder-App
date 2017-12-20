d8983cd5b40bdf7673c5740195050369caad30ea
Ramsha Hasan <ramsha.hasan2@gmail.com>
2017-12-20 15:25:12 +0500

    Add files via upload

---
+++ year finder/js/main.js
@@ -0,0 +1,13 @@
+
+document.getElementById('btn').addEventListener('submit',calculateYear);
+function calculateYear()
+{
+    var age = document.getElementById('age').value;
+   
+    var date = new Date();
+    var year = date.getFullYear();
+    
+    var birthYear = year - age;
+    
+    document.getElementById('demo').innerHTML = "Your birth year is "+birthYear+".";
+}
